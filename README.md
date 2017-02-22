4682865574fc0f4625e5813f264c1544ddf8a7f2
Luciano Ramalho <luciano@ramalho.org>
2017-02-22 19:41:46 -0300

    working on vector Add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # linalg
-Basic linear algebra operations with vectors and matrices
+Basic linear algebra operations with vectors and matrices.
+
+Inspired by examples in Chapter 4 of [Data Science from Scratch](http://shop.oreilly.com/product/0636920033400.do) by Joel Grus
--- vector/vector.go
+++ vector/vector.go
@@ -1,10 +1,14 @@
 package vector
 
+//Add adds two vectors elementwise
 func Add(v1, v2 []float64) []float64 {
-  result := make([]float64, len(v1))
+	// if len(v1) != len(v2) {
+	// 	panic(fmt.Sprintf("len(v1) != len(v2) [%d, %d]", len(v1), len(v2)))
+	// }
+	result := make([]float64, len(v1))
 
-  for i, x := range v1 {
-    result[i] = x + v2[i]
-  }
-  return result
+	for i, x := range v1 {
+		result[i] = x + v2[i]
+	}
+	return result
 }
