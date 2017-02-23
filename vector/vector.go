aec73841993e36e0eb765ed6df0058f7844d5575
Luciano Ramalho <luciano@ramalho.org>
2017-02-23 00:08:08 -0300

    all vector functions of chapter 4 implemented

--- vector/vector.go
+++ vector/vector.go
@@ -1,14 +1,87 @@
 package vector
 
-//Add adds two vectors elementwise
-func Add(v1, v2 []float64) []float64 {
-	// if len(v1) != len(v2) {
-	// 	panic(fmt.Sprintf("len(v1) != len(v2) [%d, %d]", len(v1), len(v2)))
-	// }
-	result := make([]float64, len(v1))
+import (
+	"fmt"
+	"math"
+)
 
+type operator func(a, b float64) float64
+
+//pairwise applies operator to pairs made from corresponding items in vectors
+func pairwise(op operator, v1, v2 []float64) []float64 {
+	if len(v1) != len(v2) {
+		panic(fmt.Sprintf("Vector lengths are different: %d != %d", len(v1), len(v2)))
+	}
+	result := make([]float64, len(v1))
 	for i, x := range v1 {
-		result[i] = x + v2[i]
+		result[i] = op(x, v2[i])
 	}
 	return result
+}
+
+//Add adds two vectors elementwise
+func Add(v1, v2 []float64) []float64 {
+	return pairwise(func(a, b float64) float64 { return a + b }, v1, v2)
+}
+
+//Sub subtracts two vectors elementwise
+func Sub(v1, v2 []float64) []float64 {
+	return pairwise(func(a, b float64) float64 { return a - b }, v1, v2)
+}
+
+//Sum adds vectors
+func Sum(vs ...[]float64) []float64 {
+	result := make([]float64, len(vs[0]))
+	for _, v := range vs {
+		result = Add(result, v)
+	}
+	return result
+}
+
+//ScalarMul multiplies a vector by a scalar
+func ScalarMul(x float64, v []float64) []float64 {
+	result := make([]float64, len(v))
+	for i, y := range v {
+		result[i] = x * y
+	}
+	return result
+}
+
+//Mean computes the mean of vectors (items are mean of corresponding items)
+func Mean(vs ...[]float64) []float64 {
+	return ScalarMul(1.0/float64(len(vs)), Sum(vs...))
+}
+
+//mul multiplies two vectors elementwise (used by Dot)
+func mul(v1, v2 []float64) []float64 {
+	return pairwise(func(a, b float64) float64 { return a * b }, v1, v2)
+}
+
+//Dot computes the dot product of two vectors
+func Dot(v1, v2 []float64) float64 {
+	result := 0.0
+	for _, x := range mul(v1, v2) {
+		result += x
+	}
+	return result
+}
+
+//SumOfSquares computes the dot product v · v
+func SumOfSquares(v []float64) float64 {
+	return Dot(v, v)
+}
+
+//Magnitude of the vector
+func Magnitude(v []float64) float64 {
+	return math.Sqrt(SumOfSquares(v))
+}
+
+//SquaredDistance computes the squared distance of two vectors (used by Distance)
+func SquaredDistance(v1, v2 []float64) float64 {
+	return SumOfSquares(Sub(v1, v2))
+}
+
+//Distance computes the squared distance of two vectors (used by Distance)
+func Distance(v1, v2 []float64) float64 {
+	return math.Sqrt(SquaredDistance(v1, v2))
 }
--- vector/vector_test.go
+++ vector/vector_test.go
@@ -1,7 +1,10 @@
 package vector
 
 import (
+	"fmt"
+	"math"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -12,6 +15,7 @@ func TestAdd(t *testing.T) {
 		want []float64
 	}{
 		{[]float64{1.0, 1.0}, []float64{0.0, 0.0}, []float64{1.0, 1.0}},
+		{[]float64{1.0, 2.0, 3.0}, []float64{4.0, 5.0, 6.0}, []float64{5.0, 7.0, 9.0}},
 	}
 	for _, c := range cases {
 		got := Add(c.v1, c.v2)
@@ -22,25 +26,182 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-func assertPanic(t *testing.T, f func()) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("The code did not panic")
-		}
-	}()
-	f()
-}
-
-func TestAdd_whenVectorLensAreDifferent(t *testing.T) {
-	v1 := []float64{1.0, 2.0}
-	v2 := []float64{1.0}
+func TestAdd_panicWhenVectorLengthsDiffer(t *testing.T) {
+	v1 := []float64{1.0}
+	v2 := []float64{1.0, 2.0}
 
 	defer func() {
 		if r := recover(); r == nil {
 			t.Errorf("Add(%v, %v) did not panic.", v1, v2)
+		} else if !strings.HasPrefix(fmt.Sprint(r), "Vector lengths are different") {
+			t.Errorf("Add(%v, %v): unexpected panic %q", v1, v2, r)
 		}
 	}()
 
 	Add(v1, v2)
+}
 
+func TestSub(t *testing.T) {
+	cases := []struct {
+		v1   []float64
+		v2   []float64
+		want []float64
+	}{
+		{[]float64{1.0, 1.0}, []float64{0.0, 0.0}, []float64{1.0, 1.0}},
+		{[]float64{1.0, 2.0, 3.0}, []float64{4.0, 5.0, 6.0}, []float64{-3.0, -3.0, -3.0}},
+	}
+	for _, c := range cases {
+		got := Sub(c.v1, c.v2)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Add(%v, %v) want: %v; got: %v",
+				c.v1, c.v2, c.want, got)
+		}
+	}
+}
+
+func TestSum(t *testing.T) {
+	cases := []struct {
+		vs   [][]float64
+		want []float64
+	}{
+		{[][]float64{[]float64{1.0, 2.0}, []float64{3.0, 4.0}, []float64{5.0, 6.0}},
+			[]float64{9.0, 12.0}},
+		{[][]float64{[]float64{1.0, 2.0}},
+			[]float64{1.0, 2.0}},
+	}
+	for _, c := range cases {
+		got := Sum(c.vs...)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Sum(%v) want: %v; got: %v",
+				c.vs, c.want, got)
+		}
+	}
+}
+
+func TestScalarMul(t *testing.T) {
+	cases := []struct {
+		x    float64
+		v    []float64
+		want []float64
+	}{
+		{2.0, []float64{3.0, 4.0}, []float64{6.0, 8.0}},
+		{0.0, []float64{3.0, 4.0, 5.0}, []float64{0.0, 0.0, 0.0}},
+		{1.0, []float64{}, []float64{}},
+	}
+	for _, c := range cases {
+		got := ScalarMul(c.x, c.v)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("ScalarMul(%v, %v) want: %v; got: %v",
+				c.x, c.v, c.want, got)
+		}
+	}
+}
+
+func TestMean(t *testing.T) {
+	cases := []struct {
+		vs   [][]float64
+		want []float64
+	}{
+		{[][]float64{[]float64{1.0, 2.0}, []float64{3.0, 4.0}, []float64{5.0, 6.0}},
+			[]float64{3.0, 4.0}},
+		{[][]float64{[]float64{1.0, 2.0}},
+			[]float64{1.0, 2.0}},
+	}
+	for _, c := range cases {
+		got := Mean(c.vs...)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Mean(%v) want: %v; got: %v",
+				c.vs, c.want, got)
+		}
+	}
+}
+
+func TestDot(t *testing.T) {
+	cases := []struct {
+		v1   []float64
+		v2   []float64
+		want float64
+	}{
+		{[]float64{1.0, 1.0}, []float64{0.0, 0.0}, 0.0},
+		{[]float64{1.0, 2.0, 3.0}, []float64{4.0, 5.0, 6.0}, 32.0},
+	}
+	for _, c := range cases {
+		got := Dot(c.v1, c.v2)
+		if got != c.want {
+			t.Errorf("Dot(%v, %v) want: %v; got: %v",
+				c.v1, c.v2, c.want, got)
+		}
+	}
+}
+
+func TestSumOfSquares(t *testing.T) {
+	cases := []struct {
+		v    []float64
+		want float64
+	}{
+		{[]float64{3.0, 4.0}, 25.0},
+		{[]float64{1.0, 2.0, 3.0}, 14.0},
+	}
+	for _, c := range cases {
+		got := SumOfSquares(c.v)
+		if got != c.want {
+			t.Errorf("SumOfSquares(%v) want: %v; got: %v",
+				c.v, c.want, got)
+		}
+	}
+}
+
+func TestMagnitude(t *testing.T) {
+	cases := []struct {
+		v    []float64
+		want float64
+	}{
+		{[]float64{3.0, 4.0}, 5.0},
+		{[]float64{1.0, 2.0, 3.0}, math.Sqrt(14.0)},
+	}
+	for _, c := range cases {
+		got := Magnitude(c.v)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Magnitude(%v) want: %v; got: %v",
+				c.v, c.want, got)
+		}
+	}
+}
+
+func TestSquaredDistance(t *testing.T) {
+	cases := []struct {
+		v1   []float64
+		v2   []float64
+		want float64
+	}{
+		{[]float64{1.0, 1.0}, []float64{0.0, 0.0}, 2.0},
+		{[]float64{3.0, 0.0}, []float64{0.0, 4.0}, 25.0},
+		{[]float64{1.0, 2.0, 3.0}, []float64{4.0, 5.0, 6.0}, 27.0},
+	}
+	for _, c := range cases {
+		got := SquaredDistance(c.v1, c.v2)
+		if got != c.want {
+			t.Errorf("SquaredDistance(%v, %v) want: %v; got: %v",
+				c.v1, c.v2, c.want, got)
+		}
+	}
+}
+
+func TestDistance(t *testing.T) {
+	cases := []struct {
+		v1   []float64
+		v2   []float64
+		want float64
+	}{
+		{[]float64{1.0, 1.0}, []float64{1.0, 2.0}, 1.0},
+		{[]float64{3.0, 0.0}, []float64{0.0, 4.0}, 5.0},
+		{[]float64{1.0, 2.0, 3.0}, []float64{4.0, 5.0, 6.0}, math.Sqrt(27.0)},
+	}
+	for _, c := range cases {
+		got := Distance(c.v1, c.v2)
+		if got != c.want {
+			t.Errorf("Distance(%v, %v) want: %v; got: %v",
+				c.v1, c.v2, c.want, got)
+		}
+	}
 }
